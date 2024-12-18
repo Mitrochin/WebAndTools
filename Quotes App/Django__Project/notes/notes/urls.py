839f8a40b04038826d7a15360cc24d496f80c691
Mitrochin <167693989+Mitrochin@users.noreply.github.com>
2024-12-18 14:42:10 +0100

    Update urls.py

--- Quotes App/Django__Project/notes/notes/urls.py
+++ Quotes App/Django__Project/notes/notes/urls.py
@@ -3,15 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('quotes_app.urls', namespace='quotes_app')),  # Здесь мы включаем маршруты приложения quotes_app
+    path('', include('quotes_app.urls', namespace='quotes_app')), 
 ]
-
-
-
-
-
-
-
-
-
 
